6524548fda7d02d480fad87e1cae4f458380987d
Abishek-Am26 <25bcnb02@kristujayanti.com>
2026-02-07 15:53:19 +0530

    updated the script file

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("contactForm").addEventListener("submit", function(e){
     let email = document.getElementById("email").value;
     let message = document.getElementById("message").value;
 
-    fetch("https://portfolio-project-iyar.onrender.com/submit", {
+    fetch("https://project-repo-61np.onrender.com/", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({ name, email, message })
