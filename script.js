24d3122e019939db7f23b111165fa2231cccb3c1
Abishek-Am26 <25bcnb02@kristujayanti.com>
2026-03-25 14:20:55 +0530

    updated script file

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
 document.getElementById("contactForm").addEventListener("submit", async function(e) {
     e.preventDefault();
 
-   
     const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
     const message = document.getElementById("message").value.trim();
     const msgEl = document.getElementById("msg");
 
-   
     if (!name || !email || !message) {
         msgEl.innerText = "All fields are required!";
         msgEl.style.color = "#ff5555";
@@ -16,14 +13,12 @@ document.getElementById("contactForm").addEventListener("submit", async function
     }
 
     try {
-       
         const response = await fetch("https://project-repo-c22a.onrender.com/submit", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name, email, message })
         });
 
-        
         const data = await response.json();
 
         if (response.ok && data.success) {
@@ -36,7 +31,6 @@ document.getElementById("contactForm").addEventListener("submit", async function
         }
 
     } catch (err) {
-        
         console.error("Fetch error:", err);
         msgEl.innerText = "Server error! Please try again later.";
         msgEl.style.color = "#ff5555";
